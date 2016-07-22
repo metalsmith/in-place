343df0983b93923a4067d1a4fec336846ae3939d
Ismay <ismay@ismaywolff.nl>
2016-07-22 11:41:22 +0200

    Modularize

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Local
+ */
+
+const checkFile = require('./lib/check-file');
+const renderFile = require('./lib/render-file');
+const saveFile = require('./lib/save-file');
+
+/**
+ * Main export
+ */
+
+module.exports = function plugin(opts) {
+  opts = opts || {};
+
+  // Parse options and set defaults
+  const pattern = opts.pattern || '**';
+  const options = opts.options || {};
+
+  // Main plugin function
+  return function (files, metalsmith, done) {
+    // Get data from current metalsmith instance
+    const metadata = metalsmith.metadata();
+    const source = metalsmith.source();
+
+    // Loop through all files
+    Object.keys(files).forEach((filename) => {
+      // Check file, skip processing if it fails
+      if (!checkFile(filename, pattern)) return done();
+
+      // Transform file
+      const results = renderFile(files, filename, metadata, source, options);
+
+      // Save results
+      saveFile(files, filename, results, done);
+    });
+  };
+};
