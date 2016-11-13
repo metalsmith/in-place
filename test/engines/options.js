b794ba5b2331bb0b35773833627eba063e83be13
ismay <ismay@ismaywolff.nl>
2016-11-13 15:01:30 +0100

    Add checks and update tests

---
+++ test/engines/options.js
@@ -0,0 +1,17 @@
+// Mock engine for testing, always renders the passed options into each file it processes.
+class Engine {
+  constructor(files, metalsmith, options) {
+    this.files = files;
+    this.options = options;
+  }
+
+  render(filename) {
+    return new Promise((resolve) => {
+      this.files[filename].contents = new Buffer(JSON.stringify(this.options));
+
+      resolve();
+    });
+  }
+}
+
+module.exports = Engine;
--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 // Enable --harmony flag if needed
 require('harmonize')();
 
@@ -6,13 +8,14 @@ const expect = require('expect');
 const Metalsmith = require('metalsmith');
 const path = require('path');
 const rimraf = require('rimraf');
-const buildEngine = require('./build-engine');
-const errorEngine = require('./error-engine');
+const BuildEngine = require('./engines/build');
+const ErrorEngine = require('./engines/error');
+const OptionsEngine = require('./engines/options');
 const plugin = require('../lib');
 
 describe('metalsmith-in-place', () => {
   it('should process a single file', (done) => {
-    const base = path.join('test', 'fixtures', 'single');
+    const base = path.join('test', 'fixtures', 'single-file');
     const actual = path.join(base, 'build');
     const expected = path.join(base, 'expected');
     const metalsmith = new Metalsmith(base);
@@ -20,7 +23,7 @@ describe('metalsmith-in-place', () => {
     rimraf.sync(actual);
 
     return metalsmith
-      .use(plugin({ engine: buildEngine }))
+      .use(plugin({ engine: BuildEngine }))
       .build((err) => {
         if (err) return done(err);
         equal(actual, expected);
@@ -29,7 +32,7 @@ describe('metalsmith-in-place', () => {
   });
 
   it('should process multiple files', (done) => {
-    const base = path.join('test', 'fixtures', 'multiple');
+    const base = path.join('test', 'fixtures', 'multiple-files');
     const actual = path.join(base, 'build');
     const expected = path.join(base, 'expected');
     const metalsmith = new Metalsmith(base);
@@ -37,7 +40,7 @@ describe('metalsmith-in-place', () => {
     rimraf.sync(actual);
 
     return metalsmith
-      .use(plugin({ engine: buildEngine }))
+      .use(plugin({ engine: BuildEngine }))
       .build((err) => {
         if (err) return done(err);
         equal(actual, expected);
@@ -46,7 +49,7 @@ describe('metalsmith-in-place', () => {
   });
 
   it('should only process files that match the pattern', (done) => {
-    const base = path.join('test', 'fixtures', 'pattern');
+    const base = path.join('test', 'fixtures', 'pattern-process');
     const actual = path.join(base, 'build');
     const expected = path.join(base, 'expected');
     const metalsmith = new Metalsmith(base);
@@ -56,7 +59,7 @@ describe('metalsmith-in-place', () => {
     return metalsmith
       .use(plugin({
         pattern: '*.md',
-        engine: buildEngine,
+        engine: BuildEngine,
       }))
       .build((err) => {
         if (err) return done(err);
@@ -66,7 +69,7 @@ describe('metalsmith-in-place', () => {
   });
 
   it('should fall back to the default engine', (done) => {
-    const base = path.join('test', 'fixtures', 'engine');
+    const base = path.join('test', 'fixtures', 'default-engine');
     const actual = path.join(base, 'build');
     const expected = path.join(base, 'expected');
     const metalsmith = new Metalsmith(base);
@@ -83,7 +86,7 @@ describe('metalsmith-in-place', () => {
   });
 
   it('should throw an error for invalid options', (done) => {
-    const base = path.join('test', 'fixtures', 'invalid');
+    const base = path.join('test', 'fixtures', 'invalid-options');
     const message = 'invalid options, this plugin expects a single options object.';
     const metalsmith = new Metalsmith(base);
 
@@ -96,15 +99,105 @@ describe('metalsmith-in-place', () => {
       });
   });
 
-  it('should catch errors from the engine', (done) => {
-    const base = path.join('test', 'fixtures', 'error');
+  it('should throw an error for an invalid pattern', (done) => {
+    const base = path.join('test', 'fixtures', 'invalid-pattern');
+    const message = 'invalid pattern, the pattern option should be a string.';
     const metalsmith = new Metalsmith(base);
 
     return metalsmith
-      .use(plugin({ engine: errorEngine }))
+      .use(plugin({
+        pattern: () => {},
+      }))
+      .build((err) => {
+        expect(err).toBeA(Error);
+        expect(err.message).toEqual(message);
+        return done();
+      });
+  });
+
+  it('should throw an error for an invalid engine', (done) => {
+    const base = path.join('test', 'fixtures', 'invalid-engine');
+    const message = 'invalid engine, the engine should be a constructor.';
+    const metalsmith = new Metalsmith(base);
+
+    return metalsmith
+      .use(plugin({
+        engine: 'invalid engine',
+      }))
+      .build((err) => {
+        expect(err).toBeA(Error);
+        expect(err.message).toEqual(message);
+        return done();
+      });
+  });
+
+  it('should throw an error for an engine without a render method', (done) => {
+    const base = path.join('test', 'fixtures', 'missing-render');
+    const message = 'invalid engine, the engine instance should have a render method.';
+    const metalsmith = new Metalsmith(base);
+
+    return metalsmith
+      .use(plugin({
+        engine: () => {},
+      }))
+      .build((err) => {
+        expect(err).toBeA(Error);
+        expect(err.message).toEqual(message);
+        return done();
+      });
+  });
+
+  it('should throw an error for an engine with an invalid render property', (done) => {
+    const base = path.join('test', 'fixtures', 'invalid-render');
+    const message = 'invalid engine, the render property should be a function.';
+    const metalsmith = new Metalsmith(base);
+    function Engine() {
+      this.render = 'Invalid render prop';
+    }
+
+    return metalsmith
+      .use(plugin({
+        engine: Engine,
+      }))
+      .build((err) => {
+        expect(err).toBeA(Error);
+        expect(err.message).toEqual(message);
+        return done();
+      });
+  });
+
+  it('should catch errors from the engine', (done) => {
+    const base = path.join('test', 'fixtures', 'engine-error');
+    const metalsmith = new Metalsmith(base);
+
+    return metalsmith
+      .use(plugin({ engine: ErrorEngine }))
       .build((err) => {
         expect(err).toBeA(Error);
         expect(err.message).toEqual('Message');
+        return done();
+      });
+  });
+
+  it('should pass the engine options to the engine', (done) => {
+    const base = path.join('test', 'fixtures', 'engine-options');
+    const actual = path.join(base, 'build');
+    const expected = path.join(base, 'expected');
+    const metalsmith = new Metalsmith(base);
+
+    rimraf.sync(actual);
+
+    return metalsmith
+      .use(plugin({
+        engine: OptionsEngine,
+        engineOptions: {
+          optionOne: 'value',
+          optionTwo: 'value',
+        },
+      }))
+      .build((err) => {
+        if (err) return done(err);
+        equal(actual, expected);
         return done();
       });
   });
