35d0e31c1816e1651b15aded11b9909732d51833
ismay <ismay@ismaywolff.nl>
2017-05-11 12:04:30 +0200

    Add jest

--- test/engines/error.js
+++ test/engines/error.js
@@ -1,7 +1,7 @@
 // Mock engine for testing, always returns an error
 class Engine {
   render() {
-    return Promise.reject(new Error('Message'));
+    return Promise.reject(new Error('Default error from ErrorEngine'));
   }
 }
 
